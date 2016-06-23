c53adc147fc8aede95cb9a09701cd20f960d9ee4
emmanouil <potetsia@enst.fr>
2016-06-23 11:26:27 +0200

    formatting

--- maps.js
+++ maps.js
@@ -1,21 +1,21 @@
 var map;
 
 function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
+    map = new google.maps.Map( document.getElementById( 'map' ), {
         center: {
             lat: -34.397,
             lng: 150.644
         },
         zoom: 8
-/**
- Zoom levels (approx.)
- 
-    1: World
-    5: Landmass/continent
-    10: City
-    15: Streets
-    20: Buildings
-
- */
+		/**
+		 Zoom levels (approx.)
+		 
+			1: World
+			5: Landmass/continent
+			10: City
+			15: Streets
+			20: Buildings
+		
+		 */
     });
 }
