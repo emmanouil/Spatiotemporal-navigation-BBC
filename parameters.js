b1f6da890464bed281c1f4c306f00049a2c0714b
emmanouil <potetsia@enst.fr>
2016-07-05 17:04:13 +0200

    Added parameters.js holding and referencing Global variables

---
+++ parameters.js
@@ -0,0 +1,50 @@
+/* Display Default Google Maps Marker for each Location Point */
+var USE_DEFAULT_MARKERS = true;
+
+/* Zoom Level [0,20] when viewing videos */
+var DEFAULT_ZOOM = 20;
+
+/* Show/Hide Highlight for current Location */
+var ENABLE_HIGHLIGHTER = false;
+
+
+/* Other Global vars that hold/set elements: */
+
+/**
+ *  in maps. js
+ *
+ *  map         (Object) Holds Google Map
+ *  test_icon   (Object) Holds SVG icon for camera markers
+ **/
+
+/**
+ *  in helper.js
+ *
+ *  globalSetIndex  (Array of Objects) Holds all Location/Sensor data
+ **/
+
+/**
+ *  in video.js
+ *
+ *  curr_set        (Object)    DURING PLAYBACK Holds the Object from "globalSetIndex" for current video
+ *  curr_set_index  (int)       DURING PLAYBACK index of curr_set at current time
+ *  curr_timeout, isPlaying
+ **/
+
+/**
+ *  in script.js
+ *
+ *  active_video_id (String)    The id of selected file
+ *  mediaSource                 Not used for now
+ *  playlist        (Array of Strings)  Holds IDs
+ *  video           (Element)   The video
+ *  selector        (Element)   The "Select File"
+ *
+ * File-in related vars:
+ * input_dir = 'parsing'
+ * playlist_file = 'playlist.txt'
+ * pl_element_prefix = 'OUT_'
+ * pl_video_prefix = 'VID_'
+ * pl_element_extension = '.txt'
+ * pl_video_extension = '.mp4'
+ **/
