61e663977b453f8387e9941606ae6183a98374c2
emmanouil <potetsia@enst.fr>
2016-08-31 16:42:05 +0200

    moved non-helper functions to script.js fixed initial map centering issue

--- helper.js
+++ helper.js
@@ -21,33 +21,6 @@ function degToRad(rad_in) {
     return rad_in * (Math.PI / 180);
 }
 
-function addOption(value, text) {
-    var option = document.createElement("option");
-    if (text) {
-        option.text = text;
-        if (!selector.length) {
-            active_video_id = text;
-        }
-    } else {
-        logERR("text required - used as video id");
-    }
-    option.value = value;
-    selector.add(option);
-}
-
-//returns loc_obj
-function addToIndex(XMLHttpRequest_in) {
-    var loc_obj = new Object();
-    loc_obj.index = globalSetIndex.length;
-    loc_obj.textFileURL = XMLHttpRequest_in.responseURL;
-    loc_obj.id = loc_obj.textFileURL.slice(loc_obj.textFileURL.indexOf(pl_element_prefix) + pl_element_prefix.length, loc_obj.textFileURL.indexOf(pl_element_extension));
-    loc_obj.textFile = pl_element_prefix + loc_obj.id + pl_element_extension;
-    loc_obj.videoFile = pl_video_prefix + loc_obj.id + pl_video_extension;
-    loc_obj.set = XMLHttpRequest_in.response;
-    globalSetIndex.push(loc_obj);
-    return loc_obj;
-}
-
 function getSetByVideoId(id_in) {
     for (item in globalSetIndex) {
         if (globalSetIndex[item].id === id_in.toString())
--- script.js
+++ script.js
@@ -7,7 +7,7 @@ var pl_element_prefix = 'OUT_';
 var pl_video_prefix = 'VID_';
 var pl_element_extension = '.txt';
 var pl_video_extension = '.mp4';
-var active_video_id;
+var active_video_id = null;
 var mediaSource = new MediaSource();	//Not used for now
 //var skeleton_worker = new Worker( 'parser.js' );
 var selector, video, playlist;
@@ -69,17 +69,44 @@ function parse_pl_element() {
 	if( this.status == 200 ) {
 		var tmp_obj = addToIndex(this);	//add to globalSetIndex
 		addOption(input_dir + '/'+tmp_obj.videoFile, tmp_obj.id);	//add option to the dropdown
-		addMarkers(tmp_obj.set, tmp_obj.index);
+		addMarkers(tmp_obj.set, tmp_obj.index, tmp_obj.id);
 	}
 	console.log( this )
 }
 
-function addMarkers(loc_set, index){
+function addMarkers(loc_set, index, file_id){
 	var tmp = loc_set[ 0 ].Location;
-	centerMap( tmp.Latitude, tmp.Longitude, current_zoom );
+	if(active_video_id == null){
+	    active_video_id = file_id;
+		centerMap( tmp.Latitude, tmp.Longitude, current_zoom );
+	}
 	for(var i =0; i<loc_set.length; i++){
 		if(loc_set[i].Location){
 			addMarker(loc_set[i].Location.Latitude, loc_set[i].Location.Longitude, index, loc_set[i].Sensor.DurationTotal-loc_set[i].Sensor.Duration, "Marker "+i, radToDeg(loc_set[i].Sensor.X)-90);
 		}
 	}
+}
+
+function addOption(value, file_id) {
+    var option = document.createElement("option");
+    if (file_id) {
+        option.text = file_id;
+    } else {
+        logERR("text required - used as video id");
+    }
+    option.value = value;
+    selector.add(option);
+}
+
+//returns loc_obj
+function addToIndex(XMLHttpRequest_in) {
+    var loc_obj = new Object();
+    loc_obj.index = globalSetIndex.length;
+    loc_obj.textFileURL = XMLHttpRequest_in.responseURL;
+    loc_obj.id = loc_obj.textFileURL.slice(loc_obj.textFileURL.indexOf(pl_element_prefix) + pl_element_prefix.length, loc_obj.textFileURL.indexOf(pl_element_extension));
+    loc_obj.textFile = pl_element_prefix + loc_obj.id + pl_element_extension;
+    loc_obj.videoFile = pl_video_prefix + loc_obj.id + pl_video_extension;
+    loc_obj.set = XMLHttpRequest_in.response;
+    globalSetIndex.push(loc_obj);
+    return loc_obj;
 }
