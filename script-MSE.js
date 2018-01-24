7913ad38657ead40bedf5c1c535a17a03a2b5bd9
convoitise EX <potetsia@enst.fr>
2018-01-24 15:57:19 +0100

    updated MediaSource test script

--- script-MSE.js
+++ script-MSE.js
@@ -1,88 +1,223 @@
-'use strict';
+'use strict'
+/*	This file does the initializing, generic playlist parsing and the MSE actions
+ *	For coord-parsing and other function are in processor.js
+ *
+ *	Timeline for script.js:
+ *	We first fetch the playlist
+ *	Then the MSE is opened
+ *	When the sourceopene is fired we feed the first element of the playlist (we assume to be the init .mp4 file)
+ *	After that for each playlist element we check if its coords or segment
+ *	And appendNextMediaSegment or handleCoordSet is called
+ */
 
-var FILE = '20140325_121238.webm';
-var NUM_CHUNKS = 50;
-var video = document.querySelector('video');
+//options
+const WITH_COORDS_IN_PL = true;	//backwards compatibility - to be removed
+const FULL_LOGS = false;
+const PLAYLIST_UPDATE_RATE = 500;	//in ms
+var port = '8000';
+var playlist_dir = '../out/playlist.m3u8';
+var seg_url = 'http://localhost:' + port + '/x64/Debug/out/';
+const DISABLE_AUDIO = true;
 
-if (!window.MediaSource) {
-	alert('The MediaSource API is not available on this platform');
-}
-
+//vars
+var mime_codec = 'video/mp4; codecs="avc1.42c01e"';
 var mediaSource = new MediaSource();
+var sourceBuffer;
+var video, playlist, textTrack, cues;
+var playlistArray, playlistPosition = 0;
+var req_status = -10;
+var pl_timer_ID;
+var kill_all = false;
 
-document.querySelector('[data-num-chunks]').textContent = NUM_CHUNKS;
-
-video.src = window.URL.createObjectURL(mediaSource);
-
-mediaSource.addEventListener('sourceopen', function () {
-	var sourceBuffer =
-		mediaSource.addSourceBuffer('video/webm; codecs="vorbis,vp8"');
-	console.log(sourceBuffer);
-
-	log('MediaSource readyState: ' + this.readyState);
-
-	get(FILE, function (uInt8Array) {
-		var file = new Blob([uInt8Array], {
-			type: 'video/webm'
-		});
-		var chunkSize = Math.ceil(file.size / NUM_CHUNKS);
-
-		log('Number of chunks: ' + NUM_CHUNKS);
-		log('Chunk size: ' + chunkSize + ', total size: ' + file.size);
-
-		// Slice the video into NUM_CHUNKS and append each to the media element.
-		var i = 0;
-
-		(function readChunk_(i) { // eslint-disable-line no-shadow
-			var reader = new FileReader();
-
-			// Reads aren't guaranteed to finish in the same order they're started in,
-			// so we need to read + append the next chunk after the previous reader
-			// is done (onload is fired).
-			reader.onload = function (e) {
-				sourceBuffer.appendBuffer(new Uint8Array(e.target.result));
-				log('Appending chunk: ' + i);
-				if (i === NUM_CHUNKS - 1) {
-					sourceBuffer.addEventListener('updateend', function () {
-						if (!sourceBuffer.updating && mediaSource.readyState === 'open') {
-							mediaSource.endOfStream();
-						}
-					});
-				} else {
-					if (video.paused) {
-						video.play(); // Start playing after 1st chunk is appended.
-					}
-					readChunk_(++i);
-				}
-			};
-
-			var startByte = chunkSize * i;
-			var chunk = file.slice(startByte, startByte + chunkSize);
-
-			reader.readAsArrayBuffer(chunk);
-		})(i); // Start the recursive call by self calling.
-	});
-}, false);
-
-mediaSource.addEventListener('sourceended', function () {
-	log('MediaSource readyState: ' + this.readyState);
-}, false);
-
-function get(url, callback) {
-	var xhr = new XMLHttpRequest();
-	xhr.open('GET', url, true);
-	xhr.responseType = 'arraybuffer';
-	xhr.send();
-
-	xhr.onload = function () {
-		if (xhr.status !== 200) {
-			alert('Unexpected status code ' + xhr.status + ' for ' + url);
-			return false;
-		}
-		callback(new Uint8Array(xhr.response));
-	};
+//after window loads do the init
+window.onload = function () {
+	video = document.getElementById('v');
+	mediaSource.video = video;
+	video.ms = mediaSource;
+	video.src = window.URL.createObjectURL(mediaSource);
+	fetch_pl();
+	initMSE();
 }
 
-function log(message) {
-	document.getElementById('data').innerHTML += message + '<br /><br />';
+//MSE-specific functions
+function initMSE() {
+	if (req_status == 200 && playlist.length > 0) {
+		if (mediaSource.readyState == "open") {
+			onSourceOpen();
+		} else {
+			mediaSource.addEventListener("sourceopen", onSourceOpen);
+		}
+	} else if (req_status == 200) {
+		console.log("[ABORTING] fetched playlist is empty");
+	} else {
+		console.log("waiting for playlist");
+		setTimeout(initMSE, 500);
+	}
+}
+
+function onSourceOpen() {
+
+	if (mediaSource.sourceBuffers.length > 0)
+		return;
+
+	sourceBuffer = mediaSource.addSourceBuffer(mime_codec);
+	sourceBuffer.ms = mediaSource;
+
+	//we assume the first playlist element is the location of the init segment
+	sourceBuffer.addEventListener('updateend', fetch(playlist[0], addSegment, "arraybuffer"));
+}
+
+//Append the initialization segment.
+function addSegment() {
+
+	var inSegment = this.response;
+
+	if (inSegment == null) {
+		// Error fetching the initialization segment. Signal end of stream with an error.
+		console.log("[ERROR] endofstream?")
+		mediaSource.endOfStream("network");
+		return;
+	}
+
+	sourceBuffer.appendBuffer(inSegment);
+	playlistPosition++;
+	sourceBuffer.addEventListener('updateend', handleNextPlElement, { once: false });
+}
+
+//Handle following pl elements
+function handleNextPlElement() {
+
+	if (kill_all) {
+		console.log("Playlist updates terminated");
+		return;
+	}
+	// Append some initial media data.
+	//TODO instead of terminating MSE - poll for new segs
+	if (playlistArray[playlistPosition] == null || playlistArray[playlistPosition].length < 3) {
+		//mediaSource.endOfStream();
+		console.log("[ERROR] endofplaylist?")
+		check_playlist();
+		return;
+	} else if (mediaSource.sourceBuffers[0].updating) {
+		console.log('[WARNING] MSE Buffer is still updating')
+		//		sourceBuffer.addEventListener('updateend', handleNextPlElement);
+	} else {
+		//element = playlist.splice(1, 1).toString();
+		var element = playlistArray[playlistPosition];
+		playlistPosition++;
+		if (element.endsWith('.m4s')) { //we have a segment
+			fetch(element, appendNextMediaSegment, "arraybuffer");
+			if (video.paused)
+				start_video();
+		} else if (element.endsWith('.txt')) { //we have a coordinates file
+			console.log("[WARNING] non-handled .txt element found - Check now!");
+		} else if (element.startsWith("T:")) { //we have a coordinate set file	DEPRICATED
+			console.log("[WARNING] Depricated format - Check now!");
+		} else if (element.length < 2) {
+			console.log("possible blank line in playlist - ignoring");
+			handleNextPlElement();
+		} else {
+			console.log("[WARNING] Unknown element in playlist - ignoring " + element);
+		}
+	}
+}
+
+
+function appendNextMediaSegment(frag_resp) {
+	if (FULL_LOGS) {
+		console.log("adding to SourceBuffer..")
+		console.log("size: " + frag_resp.target.response.byteLength);
+		console.log("is in updating status " + sourceBuffer.updating);
+	}
+	if (mediaSource.readyState == "closed") {
+		console.log("[ERROR] closed?")
+		return;
+	}
+	/*
+	    // If we have run out of stream data, then signal end of stream.
+	    if (!HaveMoreMediaSegments()) {
+	      mediaSource.endOfStream();
+	      return;
+	    }
+	*/
+	// Make sure the previous append is not still pending.
+	if (mediaSource.sourceBuffers[0].updating) {
+		console.log("[WARNING] previous mediaSource update still in progress")
+		return;
+	}
+
+	var mediaSegment = frag_resp.target.response;
+
+	if (!mediaSegment) {
+		// Error fetching the next media segment.
+		//mediaSource.endOfStream("network");
+		console.log("[ERROR] media segment not found");
+		return;
+	}
+
+	// NOTE: If mediaSource.readyState == “ended”, this appendBuffer() call will
+	// cause mediaSource.readyState to transition to "open". The web application
+	// should be prepared to handle multiple “sourceopen” events.
+	//	mediaSource.sourceBuffers[0].addEventListener('updateend', handleNextPlElement);
+	mediaSource.sourceBuffers[0].appendBuffer(mediaSegment);
+	if (FULL_LOGS)
+		console.log("...added")
+}
+
+//Content-loading functions
+function fetch(what, where, resp_type) {
+	console.log(v.currentTime + " fetching " + what);
+	if (what.length < 2) {
+		console.log("erroneous request");
+	}
+	var req = new XMLHttpRequest();
+	req.addEventListener("load", where);
+	req.open("GET", what);
+	if (typeof (resp_type) != 'undefined') {
+		req.responseType = resp_type;
+	}
+	req.send();
+}
+
+function fetch_pl() {
+	fetch(playlist_dir, parse_playlist);
+}
+
+function parse_playlist() {
+	playlist = this.responseText.split(/\r\n|\r|\n/); //split on break-line
+	playlistArray = playlist.slice();
+	req_status = this.status;
+}
+
+function check_playlist() {
+	fetch(playlist_dir, compare_playlist);
+}
+
+function compare_playlist() {
+	var pl_in = this.responseText.split(/\r\n|\r|\n/); //split on break-line
+	if (typeof pl_in[playlistPosition] !== 'undefined' && pl_in[playlistPosition].length > 3) {
+		if (pl_in[playlistPosition] !== playlistArray[playlistPosition]) {
+			playlistArray = pl_in.slice();
+		}
+		handleNextPlElement();
+	} else {
+		pl_timer_ID = setTimeout(check_playlist, PLAYLIST_UPDATE_RATE);
+	}
+}
+
+function start_video() {
+	console.log('play');
+	video.play();
+}
+
+function killAll() {
+	clearTimeout(pl_timer_ID);
+	kill_all = true;
+}
+
+function restartServer() {
+	var req = new XMLHttpRequest();
+	req.open('POST', '');
+	req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+	req.send('reset');
 }
